2f3bdee3b9df529a4b1db63c02fbe56b651d7a54
matlacombe <mathis@42chips.fr>
2022-02-03 22:39:26 +0100

    First commit - Temperature sensor

---
+++ test_files/esp32_client.ino
@@ -0,0 +1,43 @@
+#include <WiFi.h>
+
+/* change ssid and password according to yours WiFi*/
+const char* ssid     = "OnePlus 7";
+const char* password = "PikachuLicorne92";
+/*
+ * This is the IP address of your PC
+ * [Wins: use ipconfig command, Linux: use ifconfig command]
+*/
+const char* host = "192.168.180.204";
+const int port = 8088;
+void setup()
+{
+    Serial.begin(115200);
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    /* connect to your WiFi */
+    WiFi.begin(ssid, password);
+    /* wait until ESP32 connect to WiFi*/
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected with IP address: ");
+    Serial.println(WiFi.localIP());
+}
+void loop()
+{
+    delay(5000);
+    Serial.print("connecting to ");
+    Serial.println(host);
+    /* Use WiFiClient class to create TCP connections */
+    WiFiClient client;
+
+    if (!client.connect(host, port)) {
+        Serial.println("connection failed");
+        return;
+    }
+    /* This will send the data to the server */
+    client.print("hello world");
+    client.stop();
+}
